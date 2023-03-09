02d259c2740aaeb97425225018597d690b57f01f
Ahsan Ahmed Emon <123263533+emon4971@users.noreply.github.com>
2023-03-09 22:28:52 +0600

    Create ChangeFuel.java

---
+++ Lab 08/ChangeFuel.java
@@ -0,0 +1,10 @@
+public void changeFuel()
+        {
+        System.out.println("Changing Fuel");
+        }
+public void checkBattery()
+        {
+        System.out.println("Checking battery");
+        }
+@Override
+    }
